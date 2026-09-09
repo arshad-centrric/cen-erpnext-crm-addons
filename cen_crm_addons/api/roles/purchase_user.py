dc0f7937de075005807363903bacb247f8958fc2
midhul-centrric <midhul.intern@centrric.in>
2026-09-09 18:35:07 +0530

    reduced the long printing message in the execution command phase 2 and checked the migration

--- cen_crm_addons/api/roles/purchase_user.py
+++ cen_crm_addons/api/roles/purchase_user.py
@@ -12,6 +12,7 @@ def setup_purchase_user_role():
         }).insert(ignore_permissions=True)
     else:
 
+        pass
     # 2. Clear existing custom permissions to ensure a clean slate
     frappe.db.delete("Custom DocPerm", {"role": role_name})
 
@@ -117,6 +118,7 @@ def setup_purchase_user_role():
                 }).insert(ignore_permissions=True)
         except Exception as e:
 
+            pass
     # 5. Save to database and force cache clear
     frappe.db.commit()
     frappe.clear_cache()
